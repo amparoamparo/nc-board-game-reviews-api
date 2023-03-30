74840f1bf4a43a47764d2bea18d54df6cce115f2
amparo <114286711+amparoamparo@users.noreply.github.com>
2023-03-30 19:43:14 +0100

    update files for hosting

--- listen.js
+++ listen.js
@@ -2,10 +2,4 @@ const app = require("./app");
 
 const { PORT = 9090 } = process.env;
 
-app.listen(PORT, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`Listening on port ${PORT}...`);
-  }
-});
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
