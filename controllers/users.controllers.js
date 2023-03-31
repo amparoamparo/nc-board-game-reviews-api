e38189cf6f2dd2ae790c18dac019ecd683e9a5d0
amparo <114286711+amparoamparo@users.noreply.github.com>
2023-03-31 11:06:39 +0100

    feat: get all users

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -3,7 +3,6 @@ const { fetchAllUsers } = require("../models/users.models");
 exports.getUsers = (req, res, err) => {
   fetchAllUsers()
     .then((allUsers) => {
-      console.log(allUsers);
       res.status(200).send({ users: allUsers });
     })
     .catch((err) => {
